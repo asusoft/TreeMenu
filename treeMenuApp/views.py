51ceb0b4b1a22d615a122ce65022f37244949cc3
asusoft <ashehu19a@gmail.com>
2023-04-18 21:16:52 +0300

    Installed treeMenu App and set urls

--- treeMenuApp/views.py
+++ treeMenuApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    # Get the menu items from the database
+    # Call the draw_menu template tag to render the menu HTML
+    # Render the HTML page with the menu
+    return render(request, 'home.html')
