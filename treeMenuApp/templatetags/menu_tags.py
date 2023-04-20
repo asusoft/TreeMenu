c82fc8a63d2e244a5cd7c1da989f74edf07109c6
asusoft <ashehu19a@gmail.com>
2023-04-20 23:53:59 +0300

    Implement active item highlight

--- treeMenuApp/templatetags/menu_tags.py
+++ treeMenuApp/templatetags/menu_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from django.template import loader 
+from django.urls import reverse
 from treeMenuApp.models import Menu, MenuItem
 
 register = template.Library()
@@ -16,9 +16,12 @@ def get_menu_items(menu_name):
 def draw_menu(context, menu_name):
 
     try:
+        current_url = context['request'].path
         menu = Menu.objects.get(name=menu_name)
         menu_items = MenuItem.objects.filter(menu=menu)
-        
-        return {'menu': menu, 'menu_items': menu_items}
+        for item in menu_items:
+            item.is_active = (current_url == item.url)
+    
+        return {'menu': menu, 'menu_items': menu_items, 'current_url': current_url}
     except Menu.DoesNotExist:
         return {'menu': '', 'menu_items': ''}
