dfe8309762971f73881062d86209ff5031afabd3
asusoft <ashehu19a@gmail.com>
2023-04-20 23:12:20 +0300

    update displaying menu and menu Items

--- treeMenuApp/templatetags/menu_tags.py
+++ treeMenuApp/templatetags/menu_tags.py
@@ -1,6 +1,5 @@
 from django import template
 from django.template import loader 
-from django.urls import reverse
 from treeMenuApp.models import Menu, MenuItem
 
 register = template.Library()
@@ -13,30 +12,13 @@ def get_menu_items(menu_name):
     except Menu.DoesNotExist:
         return None
 
-@register.simple_tag
-def draw_menu(menu_name):
-    menu_items = get_menu_items(menu_name)
+@register.inclusion_tag('menu.html', takes_context=True)
+def draw_menu(context, menu_name):
 
-    result = '<ul>'
-    for item in menu_items:
-        if item.parent is None:
-            result += f'<li><a href="{item.url}">{item.name}</a>'
-            result += draw_children(item)
-            result += '</li>'
-    result += '</ul>'
-    template = loader.get_template('menu.html')
-    context = {'menu_items': result}
-    return template.render(context)
-
-def draw_children(item):
-    children = item.children.all()
-    if children:
-        result = '<ul>'
-        for child in children:
-            result += f'<li><a href="{child.url}">{child.name}</a>'
-            result += draw_children(child)
-            result += '</li>'
-        result += '</ul>'
-        return result
-    else:
-        return ''
+    try:
+        menu = Menu.objects.get(name=menu_name)
+        menu_items = MenuItem.objects.filter(menu=menu)
+        
+        return {'menu': menu, 'menu_items': menu_items}
+    except Menu.DoesNotExist:
+        return {'menu': '', 'menu_items': ''}
